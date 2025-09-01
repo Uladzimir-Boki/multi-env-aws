2ffec99a49d66a983b89cc37aab7b7da3511d4c1
Uladzimir Boki <uladzimir.boki@innowise.com>
2025-09-01 14:50:35 +0000

    minor fixes

--- keyvault.tf
+++ keyvault.tf
@@ -1,4 +1,3 @@
-# Создание секрета в AWS Secrets Manager
 resource "aws_secretsmanager_secret" "vm_root_password" {
   name        = "${var.resource_group_name}/vm-ubuntu-password"
   description = "Ubuntu password for VM"
@@ -10,20 +9,17 @@ resource "aws_secretsmanager_secret" "vm_root_password" {
   }
 }
 
-# Генерация случайного пароля
 resource "random_password" "vm_root" {
   length           = 16
   special          = true
   override_special = "!@#$%&*()-_=+[]{}<>:?"
 }
 
-# Сохранение пароля в Secrets Manager
 resource "aws_secretsmanager_secret_version" "vm_root_password" {
   secret_id     = aws_secretsmanager_secret.vm_root_password.id
   secret_string = random_password.vm_root.result
 }
 
-# Data source для получения пароля
 data "aws_secretsmanager_secret_version" "vm_root_password" {
   secret_id = aws_secretsmanager_secret.vm_root_password.id
   depends_on = [aws_secretsmanager_secret_version.vm_root_password]
